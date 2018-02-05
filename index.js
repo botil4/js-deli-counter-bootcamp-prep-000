bdbf69df8876b02d96e98df698ae73f931e56478
Travis Clarkson <tk1667@gmail.com>
2018-02-05 03:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${next}.`
   }
 }
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty"
+  } else {
+    var i = 0
+    var string = "The Line is currently: "
+    while (i < katzDeliLine.length) {
+      string = string + `${i+1}. ${katzDeliLine[i]}, `
+    }
+  }
+}
