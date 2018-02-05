c09b3ecee12adaf27b4db7fe79b81cd49b5b916c
Travis Clarkson <tk1667@gmail.com>
2018-02-05 03:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine,name) {
-  
+  katzDeliLine.push(name)
+  var len = katzDeliLine.length
+  return `Welcome, ${name}. You are number ${len} in line.`
 }
