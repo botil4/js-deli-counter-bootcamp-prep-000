182f75f19bf4f34b74a0da683b3cda51c3354b36
Travis Clarkson <tk1667@gmail.com>
2018-02-05 03:46:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    
+    var next = katzDeliLine[0]
     katzDeliLine.shift()
+    return `Currently serving ${next}.`
   }
 }
